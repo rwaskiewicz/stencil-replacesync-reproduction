a3a81a0e460379668d28245a1eb929006b1c2d94
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-09-13 13:10:43 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'happy-repro',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
